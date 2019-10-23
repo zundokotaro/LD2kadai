218b0172a730edb2474681c56b1d1f60497477d1
zundokotaro <taketake.poopoo@gmail.com>
2019-10-23 05:16:29 +0000

    課題２提出

--- kadai-2/index.js
+++ kadai-2/index.js
@@ -1,1 +1,28 @@
-console.log('hello')
+const firstName = 'Taro'
+const secondName = 'Zundoko'
+console.log(firstName + secondName)
+
+const sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+console.log(sum)
+
+const hasDog = false
+const hasCat = true
+
+//犬か猫を飼っている
+console.log(hasDog || hasCat)
+//犬も猫も飼っている
+console.log(hasDog && hasCat)
+//犬も猫も飼っていない
+console.log(!hasDog && !hasCat)
+
+const talkKing = true
+const talkSlime = true
+
+//王様またはスライムと話している
+console.log(talkKing || talkSlime)
+//王様ともスライムとも話している
+console.log(talkKing && talkSlime)
+//王様ともスライムとも話していない
+console.log(!talkKing && !talkSlime)
+//王様とは話しているがスライムとは話していない
+console.log(talkKing && !talkSlime)
