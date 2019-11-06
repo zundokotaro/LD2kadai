3469b8e12675eb201a12ebac3dcac74533301f06
zundokotaro <taketake.poopoo@gmail.com>
2019-11-06 05:32:40 +0000

    課題4追加提出

--- kadai-4/index2.js
+++ kadai-4/index2.js
@@ -14,3 +14,28 @@ setTimeout(
 3000
 )
 
+setTimeout(
+    () => {
+    console.log('発射３秒前...')
+    setTimeout(
+        () => {
+            console.log('３...')
+            setTimeout(
+                () => {
+                    console.log('２...')
+                    setTimeout(
+                        () => {
+                            console.log('１...')
+                            setTimeout(
+                                () => {
+                                    console.log('発射！')
+                                }, 1000
+                            )
+                        },1000
+                    )
+                }, 1000
+            )
+        }, 1000
+    )
+}, 3000
+)
