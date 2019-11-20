f8af509baa3c4f14e4d2626bcc0b12f63adc9478
zundokotaro <taketake.poopoo@gmail.com>
2019-11-20 04:49:51 +0000

    課題５提出

--- kadai-5/index.js
+++ kadai-5/index.js
@@ -7,5 +7,25 @@ const profile = {
     major: 'Magic',
     hobby: 'game',
     job: 'Magic layer',
-    families: '3 cats'
+    families: '2 brothers',
+    pets:{
+        name:'Popo',
+        age:12
+    }
+}
+
+const keys = Object.keys(profile)
+
+for (let  i = 0; i < keys.length; i = i + 1) {
+    const key = keys[i];
+    const value = profile[key];
+    const text = '私の'　+ key + 'は、' + value + 'です。'
+    console.log(text);
+}
+
+// of キーワードでより簡単にループができる！
+for(let key of keys){
+    const value = profile[key];
+    const text = '私の' + key + ' は、' + value + 'です。'
+    console.log(text);
 }
