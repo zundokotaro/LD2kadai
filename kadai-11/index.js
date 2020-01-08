e5255054d14f02e52bfb292f88a254cad1c9e4fb
zundokotaro <taketake.poopoo@gmail.com>
2020-01-08 05:30:48 +0000

    kadai11

---
+++ kadai-11/index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const url = require('url')
+const 
+const server = http.createServer((request, response) => {
+    const parstUrl = url.parse(req.url)
+    console.log(parseUrl)
+
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    response.write('Hello world!')
+    response.end()
+})
+server.listen(4000)
+console.log("The server is listening...")
