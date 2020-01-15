da70165b7d1453df84b3ec36b2723bb681c2c3c1
zundokotaro <taketake.poopoo@gmail.com>
2020-01-15 05:25:57 +0000

    kadai-12

---
+++ kafdai-12/index.js
@@ -0,0 +1,21 @@
+const http = require('http')
+
+
+const server = http.createServer((req, res) =>{
+    const num = Math.random()
+    res.setHeader('Access-Control-Allow-Origin','*');
+
+    if (num < 0.5) {
+        res.write('吉です')
+    } else if (num <0.7) {
+        res.write('中吉です')
+    }　else if (num < 0.95) {
+        res.write('大吉です')
+    }　else {
+        res.write('凶です')
+    }
+    
+    res.end()
+})
+server.listen(4000)
+console.log("The server is listening...")
